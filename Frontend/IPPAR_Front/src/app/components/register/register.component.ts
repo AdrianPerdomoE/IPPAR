61ca5858b681957cd141efc1c0f90c9124fbb3c1
AdrianPerdomoE <Perdomoecheverri@gmail.com>
2023-01-31 16:24:06 -0500

    registro conectado

--- Frontend/IPPAR_Front/src/app/components/register/register.component.ts
+++ Frontend/IPPAR_Front/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+import { UserService } from 'src/app/services/user-service.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  user: User = new User('', '', '', '', '')
+  savedUser: User = new User('', '', '', '', '')
+  status = ''
 
-  constructor() { }
+  constructor(private userService: UserService) {
+
+  }
 
   ngOnInit(): void {
   }
 
+  registrar() {
+    this.savedUser = this.user
+    this.userService.registerUser(this.user).subscribe(
+      response => {
+        if (response.user) {
+          this.status = "Success";
+        }
+        else {
+          this.status = "Failed";
+        }
+      }
+    )
+  }
 }

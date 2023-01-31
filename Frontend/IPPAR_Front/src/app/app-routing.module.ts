f9cf935fa830ac7611732abc1a40ae028d839d0e
alejocrrs <alejocrrs@gmail.com>
2023-01-31 11:26:13 -0500

    add register component

--- Frontend/IPPAR_Front/src/app/app-routing.module.ts
+++ Frontend/IPPAR_Front/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404Component } from './error404/error404.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: '**', component: Error404Component},
-
+  {path: 'register', component: RegisterComponent},
+  {path: '**', component: Error404Component}
 ];
 
 @NgModule({

d67b63ff15dff6399cd110ef5f4ee9d3f23116d7
AdrianPerdomoE <Perdomoecheverri@gmail.com>
2023-02-01 13:16:55 -0500

    Boton cerrar sesion

--- Frontend/IPPAR_Front/src/app/components/navbar/navbar.component.ts
+++ Frontend/IPPAR_Front/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SesionServiceService } from 'src/app/services/sesion-service.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sesionService: SesionServiceService) { }
 
   ngOnInit(): void {
+  }
+  logOut() {
+    this.sesionService.logOut()
+
   }
 
 }
--- Frontend/IPPAR_Front/src/app/services/sesion-service.service.ts
+++ Frontend/IPPAR_Front/src/app/services/sesion-service.service.ts
@@ -12,6 +12,9 @@ export class SesionServiceService {
     let userString = JSON.stringify(user)
     sessionStorage.setItem('USER', userString)
   }
+  logOut() {
+    sessionStorage.removeItem('USER');
+  }
 
   confirmOpenSesion() {
     let userString = sessionStorage.getItem('USER')

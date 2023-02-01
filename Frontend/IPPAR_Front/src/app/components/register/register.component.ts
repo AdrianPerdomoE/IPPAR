4df4994eb31b5a809531c62a0a29fa46d0b20a2a
AdrianPerdomoE <Perdomoecheverri@gmail.com>
2023-02-01 14:14:02 -0500

    verificacion del correo y campos

--- Frontend/IPPAR_Front/src/app/components/register/register.component.ts
+++ Frontend/IPPAR_Front/src/app/components/register/register.component.ts
@@ -3,6 +3,7 @@ import { User } from 'src/app/models/User';
 import { UserService } from 'src/app/services/user-service.service';
 import { Router } from '@angular/router';
 import { NotificacionesService } from 'src/app/services/notificaciones-service.service';
+import { SesionServiceService } from 'src/app/services/sesion-service.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -11,25 +12,44 @@ import { NotificacionesService } from 'src/app/services/notificaciones-service.s
 export class RegisterComponent implements OnInit {
   user: User = new User('', '', '', '', '')
   savedUser: User = new User('', '', '', '', '')
-  status = ''
+  errorCorreo = false
 
   constructor(
     private userService: UserService,
     private _router: Router,
-    private notifService: NotificacionesService
+    private notifService: NotificacionesService,
+    private sesionService: SesionServiceService
   ) {
 
   }
 
   ngOnInit(): void {
+    if (this.sesionService.confirmOpenSesion()) {
+      this._router.navigate(['/home'])
+    }
   }
 
+  analizarCorreo() {
+    this.errorCorreo = !this.verificarCorreo()
+  }
+  verificar() {
+    let verificacionCorreo = this.verificarCorreo()
+    return !verificacionCorreo || this.user.password.length < 1 || this.user.address.length<1 || this.user.name.length<1
+  }
+  verificarCorreo() {
+    let patron = new RegExp("^[a-z]+[a-z0-9._-]+@[a-z]+\.[a-z.]{2,5}$");
+    return patron.test(this.user.email)
+  }
   registrar() {
     this.savedUser = this.user
     this.userService.emailExistence(this.user.email).subscribe(
       response => {
         if (response.existence) {
-          this.status = 'UserExist';
+          this.notifService.enviarAlerta(
+            'error',
+            'Mensaje',
+            'Correo ya registrado.'
+          );
         }
         else {
           this.userService.registerUser(this.user).subscribe(
@@ -43,7 +63,6 @@ export class RegisterComponent implements OnInit {
                 );
               }
               else {
-                this.status = "Failed";
                 this.notifService.enviarAlerta(
                   'error',
                   'Mensaje',

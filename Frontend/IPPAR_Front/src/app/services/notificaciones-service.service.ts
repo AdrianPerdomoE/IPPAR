3f4422f3be5eb59a2332620a3bebe88508427679
alejocrrs <alejocrrs@gmail.com>
2023-02-01 10:49:23 -0500

    upd notif service

--- Frontend/IPPAR_Front/src/app/services/notificaciones-service.service.ts
+++ Frontend/IPPAR_Front/src/app/services/notificaciones-service.service.ts
@@ -14,6 +14,21 @@ export class NotificacionesService {
       icon: tipo,
       title: titulo,
       text: mensaje
+    });
+  }
+
+  enviarAlertaConfirmacion(title: string, mensaje: string) {
+    Swal.fire({
+      title: title,
+      text: mensaje,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Confirmar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      return result
     })
   }
 }

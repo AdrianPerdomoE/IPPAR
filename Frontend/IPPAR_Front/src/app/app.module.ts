3bde75abe9d47adb88f18049cdda72225b6cf722
alejocrrs <alejocrrs@gmail.com>
2023-02-05 14:24:29 -0500

    Fix error toastr (import BrowserAnimations module)

--- Frontend/IPPAR_Front/src/app/app.module.ts
+++ Frontend/IPPAR_Front/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
 import { HttpClientModule } from '@angular/common/http';
@@ -39,6 +40,7 @@ import { CarritoComponent } from './components/carrito/carrito.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
     ToastrModule.forRoot()
   ],
   providers: [],
